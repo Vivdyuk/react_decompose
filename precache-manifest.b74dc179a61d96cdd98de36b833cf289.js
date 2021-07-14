fed9a00113ec1837eb593b9693d99b0187197137
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-07-14 15:18:03 +0300

    Updates

---
+++ precache-manifest.b74dc179a61d96cdd98de36b833cf289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d00058df5dba092b42d9e8ce504ade",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cad1034068f499798d78",
+    "url": "./static/css/main.5e1573ad.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad1034068f499798d78",
+    "url": "./static/js/main.bcbab213.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
